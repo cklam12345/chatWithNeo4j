8dd6e3b2fe37ef0d29622251d4f8b23d853c3b72
Lam <chin-keong.lam@hp.com>
2024-11-04 14:01:53 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chatWithNeo4j
---
+++ ci.sh
@@ -0,0 +1,7 @@
+echo "# chatWithNeo4j" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:cklam12345/chatWithNeo4j.git
+git push -u origin main
